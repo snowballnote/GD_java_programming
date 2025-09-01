d0302b1bd6eb82321b6b176909ada33c5e7ebe78
snowballnote <133511707+snowballnote@users.noreply.github.com>
2025-09-01 17:36:04 +0900

    Add files via upload

--- test05/Test07.java
+++ test05/Test07.java
@@ -1,43 +1,33 @@
 package test05;
-
-import java.util.Scanner;
 /*
- * [결과]
 삼각형의 높이를 입력하세요
 3
-		9			
-	8	7	6		
-5	4	3	2	1	 
-
-5 + 3 + 1 = 9
-7 + 5 + 3 + 1 = 16
-
-1 : 1 * 2 - 1
-2 : 2 * 2 - 1
+*****
+ ***
+  *
  
-*/
-public class Test06 {
+ */
+
+import java.util.Scanner;
+
+public class Test07 {
 	public static void main(String[] args) {
 		System.out.println("삼각형의 높이");
 		Scanner sc = new Scanner(System.in);
 		int h = sc.nextInt();
-		int total = (2 * h - 1) * h / 2;
+		int count = 0;
 		for(int i = 1; i <= h; i++) {
 			//1. 공백 출력
 			for(int j = 1; j <= h - i; j++) {
 				System.out.print(" ");
 			}
-			//2. 숫자
-			for(int j = 1; j <= 2 * i - 1; j++) {
-				System.out.print(total);//??????????????????????????????
-				
+			//2. 별표찍기
+			for(int j = 2 * i - 1; j <= 1; j--) {
+				System.out.print("*");
 			}
 			
 			//줄바꿈
 			System.out.println();
 		}
 	}
-	
-		
-		
 }
---
+++ test05/Test08.java
@@ -0,0 +1,17 @@
+package test05;
+/*
+삼각형의 높이를 홀수로 입력하세요
+5
+
+*****
+ ***
+  *
+ ***
+*****
+ 
+ */
+public class Test08 {
+	public static void main(String[] args) {
+
+	}
+}
