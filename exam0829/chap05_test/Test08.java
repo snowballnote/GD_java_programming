186569412aab429963637c04390d2b9993dd002d
snowballnote <133511707+snowballnote@users.noreply.github.com>
2025-09-01 09:46:32 +0900

    Update Test08.java

--- exam0829/chap05_test/Test08.java
+++ exam0829/chap05_test/Test08.java
@@ -1,41 +1,79 @@
 package chap05_test;
 
 import java.util.Scanner;
-
 /*
 삼각형의 높이를 홀수로 입력하세요
-5
-
-*****
- ***
-  *
- ***
-*****
+5 i= 행, j= 열
+		i	j
+*****	0	0 1 2 3 4 j: 0~h-i-1 i가 0일때 j는 4 즉, 높이 마이너스 아이 마이너스 1, *: i<=j
+ ***	1	1 2 3
+  *		2	2
+ ***	3	1 2 3	j:0~i, *:
+*****	4	0 1 2 3 4
  
+ i	j
+ 3	1:h-i-1
+ 4	0:h-i-1
  */
 public class Test08 {
 	public static void main(String[] args) {
 		System.out.print("삼각형의 높이: ");
 		Scanner sc = new Scanner(System.in);
 		int h = sc.nextInt();
-		int bottom = h * 2 - 1;
-		int num = 0; //숫자의 시작 숫자 //숫자의 시작점을 알아야 1~찍을 수 있음
 		
-		for(int i = bottom; i >= 1; i -=2) {
-			num += i;
-		}
-		System.out.println("num = " + num); //시작숫자
-		
-		int m = bottom / 2; //m=5/2=2
-		for(int i = 0; i < h-2; i++) {
-			for(int j = 0; j < bottom; j++) {
-				if(j >= m-i && j <= m+i) { //??????????????????
-					System.out.print("*" + ""); //num을 찍고 숫자가 하나씩 빠짐. //별'*'로 생각하면 똑같음.
-				
+		for(int i = 0; i <= h/2; i++) {
+			for(int j = 0; j < h-i; j++)
+				if(i <= j) {
+					System.out.print("*");
 				}else {
 					System.out.print(" ");
 				}
-			}System.out.println();
+			System.out.println();
 		}
+		
+		for(int i = h/2+1; i < h; i++) {
+			for(int j = 0; j <= i; j++) {
+				if(j >= h-i-1) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+				System.out.println();
+			}
+		}
+		/*
+		int h = sc.nextInt();
+		int mid = h / 2;
+		int i = 0;
+		int j;
+		int count = h - 2 * i;
+		
+		//위쪽 삼각형
+		for(i = 0; i <= mid; i++) {
+			//공백
+			for(j = 0; j < i; j++){
+				System.out.print(" ");
+			}
+			//별
+			for(j = 0; j < count ; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		//아래쪽 삼각형
+		for(i = mid - 1; i >= 0; i--) {
+			//공백
+			for(j = 0; j < i; j++){
+				System.out.print(" ");
+			}
+			//별
+			for(j = 0; j < count ; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		sc.close();
+		*/
 	}
 }
