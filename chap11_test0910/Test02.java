dafaeca3b666bfefb60ea23d39ed70e2d25e1b51
snowballnote <133511707+snowballnote@users.noreply.github.com>
2025-09-10 23:19:29 +0900

    Update Test02.java

--- chap11_test0910/Test02.java
+++ chap11_test0910/Test02.java
@@ -6,11 +6,24 @@ package chap11_test0910;
  * basketball
  */
 public class Test02 {
-	public static void main(String[] args) {
-		String str = "base"; 
-		//str = change(str);
-		System.out.println(str.replace('e','k')+"et"); //basket
-		str += "ball"; 
-		System.out.println(str); //basketball
-	}
+    public static void main(String[] args) {
+        String str = "base"; 
+        
+        // change 메서드를 이용해서 "base" → "bask" 변환
+        str = change(str);
+        System.out.println(str + "et"); // basket
+
+        // "ball"을 이어붙여서 "basketball" 완성
+        str += "ball"; 
+        System.out.println(str); // basketball
+    }
+
+    /*
+     * change 메서드
+     * - 문자열 "base"에서 'e'를 'k'로 변경
+     * - String은 불변(immutable)이므로 replace 결과를 반환해야 함
+     */
+    public static String change(String s) {
+        return s.replace('e', 'k'); 
+    }
 }
