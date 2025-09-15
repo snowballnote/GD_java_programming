932e74cf70acf42d7965bce392e101ad661a87e0
snowballnote <133511707+snowballnote@users.noreply.github.com>
2025-09-15 09:05:44 +0900

    Update Test01.java

--- chap12_test0912/Test01.java
+++ chap12_test0912/Test01.java
@@ -11,6 +11,7 @@ package chap12_test0912;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+/***************
 public class Test01 {
 	public static void main(String[] args) {
 		String[] phoneNumArr = {
@@ -32,5 +33,34 @@ public class Test01 {
 			System.out.println();
 		}
 		
+	}
+}
+**************************/
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Test01 {
+	public static void main(String[] args) {
+		String[] phoneNumArr = {
+				"012-3456-7890","099-2456-7980", 
+				"088-2346-9870","013-3456-7890" };
+		Scanner s = new Scanner(System.in);
+		System.out.println("찾고자 하는 번호를 입력하세요:");
+		String input = s.nextLine().trim();
+		String pattern = input;
+		Pattern p = Pattern.compile(pattern);
+		int cnt = 0;
+		for (int i = 0; i < phoneNumArr.length; i++) {
+			String phoneNum = phoneNumArr[i];
+			String tmp = phoneNum.replace("-", ""); //- 제외시켜, -사이의 숫자 검색
+			Matcher m = p.matcher(tmp);
+			if (m.find()) { 
+				cnt++;
+				System.out.println(cnt + ":" + phoneNum);
+			}
+		}
+		if(cnt == 0)
+	     	System.out.println("찾는 번호가 없습니다.");
 	}
 }
