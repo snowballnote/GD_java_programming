bf07588f06f602406f0583da444d85ae4f3aae0b
snowballnote <133511707+snowballnote@users.noreply.github.com>
2025-09-15 09:11:21 +0900

    Update Test02.java

--- chap12_test0912/Test02.java
+++ chap12_test0912/Test02.java
@@ -10,6 +10,7 @@ import java.util.regex.Pattern;
   example_1@gmail.com
   contact@company.co.kr
  */
+/*****************
 public class Test02 {
 	public static void main(String[] args) {
         String text = 
@@ -28,4 +29,18 @@ public class Test02 {
         	System.out.println(m2.group());
         }
 	}
+}
+*******************************/
+public class Test02 {
+	public static void main(String[] args) {
+        String text = 
+        	"Hello, my email is example_1@gmail.com and my work email is contact-1@company.co.kr.";
+		String emailP = "([0-9A-Za-z_-]+)@([0-9A-Za-z]+).([0-9A-Za-z]+)";
+		Pattern p = Pattern.compile(emailP);
+		Matcher m = p.matcher(text);
+		System.out.println("추출된 이메일:");
+		while(m.find()) {
+			System.out.println(m.group());
+		}
+    }
 }
