62510dd095eabc37a2a339c7f866bc52b00b2c2b
snowballnote <133511707+snowballnote@users.noreply.github.com>
2025-09-16 21:39:21 +0900

    Update MainEx01.java

--- chap11/ex09_regular/MainEx01.java
+++ chap11/ex09_regular/MainEx01.java
@@ -4,30 +4,48 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /*
- * 정규표현식: 문자의 형식을 지정 : ex)이메일형식, 전화번호 형식 인지아닌지 알아볼때 사용.
+ * 정규표현식(Regular Expression)
+ * - 문자열의 형식을 지정하고, 해당 형식과 일치하는지 검사할 때 사용.
+ * - 예: 이메일, 전화번호, 특정패턴단어 찾기
  */
 public class MainEx01 {
+
 	public static void main(String[] args) {
-		String[] data = {"bat", "baby", "bonus", "c", "cA", "ca", "c.", "cpmbat", "count", "cTT", "data", "disk"};
+		//테스트용 문자열 배열
+		String[] data = {
+			"bat", "baby", "bonus", "c", "cA", "ca", "c.", 
+	        "cpmbat", "count", "cTT", "data", "disk"		
+		};
 		/*
-		 *	c[A-Za-z]*: c문자로 시작하고, 뒤쪽에는 영문자 0개이상이되는 단어를 찾는다.=> 없어도되고 0개이상이되는 //c,cA,ca,cpmbat,count,cTT,
-		 *	c[A-Za-z]: c문자로 시작하고, 뒤쪽에는 영문자 1개인 단어 선택 //cA,ca,
-		 *	c[A-Za-z]+: c문자로 시작하고, 뒤쪽에는 영문자 1개 이상되는 단어 선택 //cA,ca,cpmbat,count,cTT,
-		 *	c[A-Za-z]?: c문자로 시작하고, 뒤쪽에는 영문자 0개, 1개인 단어 선택 //c,cA,ca,
-		 *
-		 *	*: 0개이상
-		 *	+: 1개이상
-		 *	?: 0,1개
+		 * 정규표현식 예시
+		 * 	C[A-Za-z]*: c로 시작하고 뒤에는 영문자(대/소문자) 0개 이상-> c, cA, ca, cpmbat, count, cTT ...
+		 * 	C[A-Za-z] : c로 시작하고 뒤에는 영문자 딱 1개-> cA, ca
+		 * 	C[A-Za-z]+: c로 시작하고 뒤에는 영문자 1개 이상-> cA, ca, cpmbat, count, cTT
+		 *  C[A-Za-z]?: c로 시작하고 뒤에는 영문자 0개 or 1개-> c, cA, ca
+		 *  
+		 *  * : 0개 이상
+		 *  + : 1개 이상
+		 *  ? : 0개 또는 1개
 		 */
-		Pattern p = Pattern.compile("c[A-Za-z]?");
+		
+		//패턴 설정(여기서는 "c로 시작하고, 뒤에 영문자 0-1개"선택)
+		Pattern p = Pattern .compile("c[A-Za-z]?");
+		
+		//data배열에서 패턴과 일치하는 문자열만 출력
 		for(String s : data) {
-			Matcher m = p.matcher(s); //bat, baby, bonus=> c로시작하지 않아서x, c.=>c다음이 알파벳이 아니여서 안됨.
+			Matcher m = p.matcher(s);
+			//matches(): 문자열 전체가 패턴에 일치해야 true
 			if(m.matches()) System.out.print(s + ",");
-		}
+		} //c,cA,ca,
 		System.out.println();
 		
-		String names = "홍길동,     김삿갓,이몽룡,임꺽정";
-		String arr[] = names.split("\\s*,\\s*"); //공백이 0개이상.
-		for(String s: arr) System.out.println(s);
+		//정규식 split 활용 예시
+		String names = "홍길동,      김삿갓,이몽룡,임꺽정";
+		
+		//"\\s*,//s*": 콤마(,)를 기준으로 자르는데, 앞뒤에 공백(스페이스)이 0개 이상 있는 경우도 허용
+		String arr[] = names.split("\\s*,\\s*");
+		
+		for(String s: arr) System.out.println(s); //홍길동김삿갓이몽룡임꺽정
 	}
+
 }
