483a2a220d43f714ced35d7441bb34e0f1cb1afc
snowballnote <133511707+snowballnote@users.noreply.github.com>
2025-09-17 08:42:53 +0900

    Add files via upload

---
+++ chap03_test0826/Test04.java
@@ -0,0 +1,29 @@
+package chap03_test0826;
+
+import java.util.Scanner;
+
+/*
+ *  화면에서 두수를 입력받아서 더큰수를 출력하기 
+ */
+public class Test04 {
+	public static void main(String[] args) {
+		System.out.println("두수를 입력:");
+		Scanner scan = new Scanner(System.in);
+		
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
+		
+		int max;
+		if(num1 > num2) {
+			max = num1;
+			System.out.println(max);				
+		} else if (num1 < num2){
+			max = num2;
+			System.out.println(max);
+		} else {
+			System.out.println("두 수는 같다.");
+		}
+		
+		scan.close();
+	}
+}
---
+++ chap03_test0826/Test05.java
@@ -0,0 +1,29 @@
+package chap03_test0826;
+
+import java.util.Scanner;
+/*
+ * 밑변과 높이를 입력받아 삼각형의 넓이를 출력하기. 소숫점을 표시
+ * [결과]
+ * 밑변의 길이
+ * 10
+ * 높이의 길이
+ * 20
+ * 
+ * 넓이=150.0
+ */
+public class Test05 {
+	public static void main(String[] args) {
+		System.out.println("밑변의 길이");
+		Scanner sc = new Scanner(System.in);
+		int w = sc.nextInt();
+		
+		System.out.println("높이의 길이");
+		int h = sc.nextInt();
+		
+		double width = (w * h) / 2d; //이렇게하면 소수점이 탈락됨. 2.0,2d이렇게 하면 소수점 나옴.
+		System.out.println("넓이=" + width);
+		
+		sc.close();
+		
+	}
+}
