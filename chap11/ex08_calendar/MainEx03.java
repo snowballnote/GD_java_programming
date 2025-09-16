388277c219dabfe74274d8ad972242baf1caa80b
snowballnote <133511707+snowballnote@users.noreply.github.com>
2025-09-16 20:57:26 +0900

    Update MainEx03.java

--- chap11/ex08_calendar/MainEx03.java
+++ chap11/ex08_calendar/MainEx03.java
@@ -4,30 +4,43 @@ import java.util.Calendar;
 import java.util.Date;
 
 /*
- * Date객체와 연결
+ * Date 객체와 연결
  */
 public class MainEx03 {
-
 	public static void main(String[] args) {
 		//Calendar <= Date
+		//현재 일시를 가진 Date 객체 생성
 		Date now = new Date();
-		now.setTime(now.getTime() + (1000L * 60 * 60 * 24)); //내일이됨.(오늘은 목요일)
-		System.out.println(now); //Fri Sep 12 15:06:11 KST 2025
+		
+		//하루(24시간) 뒤로 이동
+		now.setTime(now.getTime() + (1000L * 60 * 60 * 24));
+		System.out.println(now);
+		//Fri Sep 12 15:06:11 KST 2025
+		
+		//Date 객체를 이용해서 Calendar 객체 설정
 		Calendar cal = Calendar.getInstance();
-		cal.setTime(now); //Date 객체를 이용하여 Calendar객체의 날짜 설정
+		cal.setTime(now); //Date -> Calendar 변환
+		
+		//요일 한글 변환용 문자열
 		String week = "일월화수목금토";
-		System.out.printf("날짜:%04d-%02d-%02d %c요일\n", cal.get(Calendar.YEAR),
-													cal.get(Calendar.MONTH)+1,
-													cal.get(Calendar.DATE),
-													week.charAt(cal.get(Calendar.DAY_OF_WEEK) - 1));
-													//날짜:2025-09-12 금요일
+		
+		//Calendar 정보를 포맷팅해서 출력
+		System.out.printf("날짜:%04d-%02d-%02d %c요일\n",
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH) + 1,   // 0부터 시작이므로 +1
+                cal.get(Calendar.DATE),
+                week.charAt(cal.get(Calendar.DAY_OF_WEEK) - 1));
+		//날짜:2025-09-12 금요일
 		
 		//Date <= Calendar
-		Date day = new Date(); //현재일시
-		//Calendar 객체를 이용하여 Date 객체의 날짜 설정하기
-		//cal: 내일의 날짜를 가지고 있는 객체
-		day.setTime(cal.getTimeInMillis()); //cal 객체의 날짜로 day객체의 날짜 설정
-		System.out.println(day); //Fri Sep 12 15:10:18 KST 2025 : cal과 day가 연결되어있어 day도 cal처럼 내일 날짜가 추력됨.
+		//현재 일시를 가진 Date 객체 생성
+		Date day = new Date();
+		
+		//Calendar객체가 가진 시간 값(ms)을 Date에 설정
+		//(즉, Calendar -> Date 변환)
+		day.setTime(cal.getTimeInMillis());
+		
+		System.out.println(day); //Fri Sep 12 15:10:18 KST 2025
+		//-> cal이 "내일" 날짜를 갖고 있으므로 day도 동일하게 "내일" 날짜 출력
 	}
-
 }
