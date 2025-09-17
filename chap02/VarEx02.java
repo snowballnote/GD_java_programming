8f23640c9502388687eae732296ab4a4535b1e73
snowballnote <133511707+snowballnote@users.noreply.github.com>
2025-09-17 23:10:32 +0900

    Update VarEx02.java

--- chap02/VarEx02.java
+++ chap02/VarEx02.java
@@ -1,36 +1,43 @@
 package chap02;
 /*
- * 문자형 char => 기본자료형. 한개의 문자만 저장 가능함. 작은 따옴표로 표현.
- * 문자열형 String => 참조자료형. 0개 이상의 문자들을 저장 가능함. 큰 따옴표로 표현. 
+ * 문자형 char => 기본자료형. 한 개의 문자만 저장 가능함. 작은 따옴표로 표현.
+ * 문자열형 String => 참조자료형. 0개 이상의 문자들을 저장 가능함. 큰 따옴표로 표현.
  */
 public class VarEx02 {
 
 	public static void main(String[] args) {
-		char c1 = 'A'; //한개의 문자만 저장 가능
-		//char c2 = 'AB'; //2개의 문자 저장 불가
-		//char c3 = ''; //최소 한개의 문자는 있어야함
-		String s1 = "A"; 
-		System.out.println("c1=" + c1);
-		System.out.println("s1=" + s1);
-		String s2 = "ABCDEFGI";
-		System.out.println("s2=" + s2);
-		String s3 = "";
-		System.out.println("s3=" + s3);
+		//char: 문자형 변수(한 글자만 저장 가능)
+		char c1 = 'A'; //'A' 저장
+		//char c2 = 'AB'; //두 글자 이상 저장 불가 -> 에러
+		//char c3 = ''; //최소 한 글자는 필요 -> 에러
 		
-		//식별자는 예약어 불가. 그래서 대문자로 변경해 변수명으로 사용. 그러나 변수명도 소문자로 시작하는 것을 권장함.
-		//char true = '참'; //error이유 : 예약어여서. => 예약어 사용 불가
-		char True = '참'; //변수명은 소문자로 시작함(권장)
+		//String: 문자열(여러 글자) 저장 가능
+		String s1 = "A"; //문자열 "A" 저장
+		System.out.println("c1=" + c1); //c1=A
+		System.out.println("s1=" + s1); //s1=A
 		
-		int max_num = 100; //권장사항. 권장하진 않음 //int maxNum = 100;으로 사용할 것.
-		//int max-num = 10000; //특수문자 '-'은 식별자로 사용 불가. 특수문자 '_, $'만 가능
+		String s2 = "ABCDEFGI"; //여러개의 문자저장 가능
+		System.out.println("s2=" + s2); //s2=ABCDEFGI
 		
-		//숫자로 시작하는 식별자 불가.
-		//int 7up = 7; //숫자 시작 안됨.
-		int top10 = 10; //숫자 포함 가능.(시작만 안됨)
+		String s3 = ""; //빈문자열도 가능
+		System.out.println("s3=" + s3); //s3=
 		
-		//_,$ 문자로는 시작 가능함.
-		char $harp = 'S';
+		//예약어는 변수명으로 사용 불가
+		//char true = '참'; //에러(예약어 사용불가)
 		
+		//대문자로 시작하면 변수명으로 사용 가능하지만 권장 X
+		char True = '참'; //변수명은 소문자로 시작하는 것이 권장
+		
+		//식별자 규칙
+		int max_num = 100; //가능하지만 카멜 표기법(int maxNum)이 권장됨
+		//int max-num = 10000; //불가능('-'사용불가)/'_','$'만 특수문자 가능
+		
+		//숫자로 시작하는 변수명 불가
+		//int 7up = 7; //에러
+		int top10 = 10; //숫자 포함 가능(단, 첫 글자만 아니면됨)
+		
+		//'_','$'로 시작하는 변수명 가능
+		char $harp = 'S'; //가능
 	}
 
 }
